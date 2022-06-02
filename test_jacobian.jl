926ee93e98ceae454e1a23024debbec11ab180c2
Indy Ng <writingindy@gmail.com>
2022-06-02 16:43:26 -0400

    Tidied up code slightly

--- test_jacobian.jl
+++ test_jacobian.jl
@@ -24,8 +24,6 @@ g(i, j, k, grid, x, y) = sin(x[i]) * cos(y[j])
 ∂g_x(i, j, k, grid, x, y) = cos(x[i])* cos(y[j])
 ∂g_y(i, j, k, grid, x, y) = -sin(x[i]) * sin(y[j])
 
-jacobian_func(i, j, k, grid, x, y) =  ∂f_x(i, j, k, grid, x, y)#= * ∂g_y(i, j, k, grid, x, y) =# #= - (∂f_y(i, j, k, grid, x, y) * ∂g_x(i, j, k, grid, x, y))=#
-
 fᶜ = zeros(Float64, length(xᶜ), length(yᶜ))
 ∂f_xᶠ = zeros(Float64, length(xᶠ), length(yᶠ))
 ∂f_yᶠ = zeros(Float64, length(xᶠ), length(yᶠ))
@@ -38,7 +36,6 @@ jacobian = zeros(Float64, length(xᶠ), length(yᶠ))
 
 jacobian_approx = zeros(Float64, length(xᶜ), length(yᶜ))
 
-# Are xᶜ and xᶠ (similarly for y) ever arrays with different lengths?
 for (index, i) in enumerate(1:Nx)
     for (index, j) in enumerate(1:Ny)
         fᶜ[i, j] = f(i, j, 1, test_grid, xᶜ, yᶜ)
@@ -57,15 +54,10 @@ for (index, i) in enumerate(1:Nx)
     end
 end
 
-#∂f_xᶠ[i] = ∂f_x(i, 1, k, grid, xᶠ , yᶠ)
-#∂f_x_grid = [∂f_x for ∂f_x in ∂f_xᶠ]
-
 
 error = abs.(jacobian .- jacobian_approx)
 
 
-#plt_f = plot_surface(xᶜ, yᶜ, fᶜ);
-#plt_g = plot_surface(xᶜ[1:Nx], yᶜ[1:Nx], gᶜ[1:(end-6), 1:(end-6)])
 plt_jacobian = plot_surface(xᶜ, yᶜ, jacobian)
 plt_jacobian_approx = plot_surface(xᶜ, yᶜ, jacobian_approx)
 #plt_error = plot_surface(, y_grid[2:end], error)
