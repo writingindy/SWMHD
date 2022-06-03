9ddcec2b8a4a27a5b36bdbcf47e46419764e43af
Indy Ng <writingindy@gmail.com>
2022-06-03 16:01:07 -0400

    Fixed minor error in MHD_visualize script

--- MHD_visualize.jl
+++ MHD_visualize.jl
@@ -3,20 +3,20 @@ using Oceananigans.Operators
 using PyPlot
 
 Lx, Ly = 10,  10
-Nx, Ny = 100, 100
+Nx, Ny = 200, 200
 
 grid = RectilinearGrid(CPU(); size = (Nx, Ny), 
                       x = (-Lx/2, Lx/2), y = (-Ly/2, Ly/2),
                       topology = (Periodic, Periodic, Flat))
 
 xᶜ, xᶠ = grid.xᶜᵃᵃ, grid.xᶠᵃᵃ
-yᶜ, yᶠ = grid.yᵃᶜᵃ, grid.yᵃᶠᵃ 
+yᶜ, yᶠ = grid.yᵃᶜᵃ, grid.yᵃᶠᵃ
 
 exp_var = 0.2
 
-   A(i, j, k, grid, x, y) = exp(-exp_var * (x[i])^2 -exp_var * (y[j])^2)
-∂A_x(i, j, k, grid, x, y) = -(exp_var * 2) * x[i] * exp(-x[i]^2 -y[j]^2)
-∂A_y(i, j, k, grid, x, y) = -(exp_var * 2) * y[j] * exp(-x[i]^2 -y[j]^2)
+   A(i, j, k, grid, x, y) = exp(-exp_var * (x[i])^2 - exp_var * (y[j])^2)
+∂A_x(i, j, k, grid, x, y) = -(exp_var * 2) * x[i] * exp(-exp_var * (x[i])^2 - exp_var * (y[j])^2)
+∂A_y(i, j, k, grid, x, y) = -(exp_var * 2) * y[j] * exp(-exp_var * (x[i])^2 - exp_var * (y[j])^2)
 
 jacobian_x_approx = zeros(Float64, length(xᶜ), length(yᶜ))
 jacobian_y_approx = zeros(Float64, length(xᶜ), length(yᶜ))
@@ -43,10 +43,16 @@ lorentz_force_y = -1 .* jacobian_x_approx
 B_x = -1 .* ∇A_y
 B_y = ∇A_x
 
-#plt_A = colorbar(contourf(xᶜ, yᶜ, A_num))
-#plt_lorentz = quiver(xᶜ, yᶜ, lorentz_force_x, lorentz_force_y, scale = 5)
-#title("Contour plot of A and quiver plot of Lorentz force")
+#plt = plot_surface(xᶜ, yᶜ, A_num, cmap=ColorMap("coolwarm"))
+
+#=plt_A = colorbar(contourf(xᶜ, yᶜ, A_num))
+plt_lorentz = quiver(xᶜ, yᶜ, lorentz_force_x, lorentz_force_y, scale = 17)
+title("Contour plot of A and quiver plot of Lorentz force")
+xlim((-4, 4))
+ylim((-4, 4))=#
 
 plt_A = colorbar(contourf(xᶜ, yᶜ, A_num))
-plt_B = quiver(xᶜ, yᶜ, B_x, B_y, scale = 50)
+plt_B = quiver(xᶜ, yᶜ, B_x, B_y, scale = 75)
 title("Contour plot of A and quiver plot of magnetic field")
+xlim((-4, 4))
+ylim((-4, 4))
