be74b57a93828f64c79f74e92a9cb9b34620ca95
Francis J. Poulin <fpoulin@uwaterloo.ca>
2022-06-03 08:22:38 -0400

    Update test_jacobian.jl
    Cleaning things up a litte but the results should be exactly the same.  If not, then I messed up somewhere.

--- test_jacobian.jl
+++ test_jacobian.jl
@@ -2,61 +2,43 @@ using Oceananigans
 using Oceananigans.Operators
 using PyPlot
 
-Lx = 10
-Ly = 10
-Nx = 400
-Ny = 400
+Lx, Ly = 10,  10
+Nx, Ny = 400, 400
 
-test_grid = RectilinearGrid(CPU(); size = (Nx, Ny), x = (-Lx/2, Lx/2), y = (-Ly/2, Ly/2),
+grid = RectilinearGrid(CPU(); size = (Nx, Ny), 
+                      x = (-Lx/2, Lx/2), y = (-Ly/2, Ly/2),
                       topology = (Periodic, Periodic, Flat))
 
-xᶜ = test_grid.xᶜᵃᵃ
-xᶠ = test_grid.xᶠᵃᵃ
+xᶜ, xᶠ = grid.xᶜᵃᵃ, grid.xᶠᵃᵃ
+yᶜ, yᶠ = grid.yᵃᶜᵃ, grid.yᵃᶠᵃ 
 
-yᶜ = test_grid.yᵃᶜᵃ
-yᶠ = test_grid.yᵃᶠᵃ
-
-f(i, j, k, grid, x, y) = exp(-x[i]^2 -y[j]^2)
+   f(i, j, k, grid, x, y) = exp(-x[i]^2 -y[j]^2)
 ∂f_x(i, j, k, grid, x, y) = -2 * x[i] * exp(-x[i]^2 -y[j]^2)
 ∂f_y(i, j, k, grid, x, y) = -2 * y[j] * exp(-x[i]^2 -y[j]^2)
 
-g(i, j, k, grid, x, y) = sin(x[i]) * cos(y[j])
+   g(i, j, k, grid, x, y) = sin(x[i]) * cos(y[j])
 ∂g_x(i, j, k, grid, x, y) = cos(x[i])* cos(y[j])
 ∂g_y(i, j, k, grid, x, y) = -sin(x[i]) * sin(y[j])
 
-fᶜ = zeros(Float64, length(xᶜ), length(yᶜ))
 ∂f_xᶠ = zeros(Float64, length(xᶠ), length(yᶠ))
 ∂f_yᶠ = zeros(Float64, length(xᶠ), length(yᶠ))
-
-gᶜ = zeros(Float64, length(xᶜ), length(yᶜ))
 ∂g_xᶠ = zeros(Float64, length(xᶠ), length(yᶠ))
 ∂g_yᶠ = zeros(Float64, length(xᶠ), length(yᶠ))
 
-jacobian = zeros(Float64, length(xᶠ), length(yᶠ))
-
+ jacobian_exact = zeros(Float64, length(xᶠ), length(yᶠ))
 jacobian_approx = zeros(Float64, length(xᶜ), length(yᶜ))
 
-for (index, i) in enumerate(1:Nx)
-    for (index, j) in enumerate(1:Ny)
-        fᶜ[i, j] = f(i, j, 1, test_grid, xᶜ, yᶜ)
-        gᶜ[i, j] = g(i, j, 1, test_grid, xᶜ, yᶜ)
-        jacobian_approx[i, j] =   ∂xᶠᶜᶜ(i, j, 1, test_grid, f, xᶜ, yᶜ) * ∂yᶜᶠᶜ(i, j, 1, test_grid, g,  xᶜ, yᶜ)  - ∂xᶠᶜᶜ(i, j, 1, test_grid, g, xᶜ, yᶜ) * ∂yᶜᶠᶜ(i, j, 1, test_grid, f, xᶜ, yᶜ)
-    end
+for i = 1:Nx, j = 1:Ny
+    jacobian_approx[i, j] = ∂xᶠᶜᶜ(i, j, 1, grid, f, xᶜ, yᶜ) * ∂yᶜᶠᶜ(i, j, 1, grid, g,  xᶜ, yᶜ)  
+                          - ∂xᶠᶜᶜ(i, j, 1, grid, g, xᶜ, yᶜ) * ∂yᶜᶠᶜ(i, j, 1, grid, f, xᶜ, yᶜ)
 end
 
-for (index, i) in enumerate(1:Nx)
-    for (index, j) in enumerate(1:Ny)
-        ∂f_xᶠ[i, j] = ∂f_x(i, j, 1, test_grid, xᶠ, yᶜ)
-        ∂f_yᶠ[i, j] = ∂f_y(i, j, 1, test_grid, xᶜ, yᶠ)
-        ∂g_xᶠ[i, j] = ∂g_x(i, j, 1, test_grid, xᶠ, yᶜ)
-        ∂g_yᶠ[i, j] = ∂g_y(i, j, 1, test_grid, xᶜ, yᶠ)
-        jacobian[i, j] = ∂f_xᶠ[i, j] * ∂g_yᶠ[i, j] - ∂f_yᶠ[i, j] * ∂g_xᶠ[i, j]
-    end
+for i = 1:Nx, j = 1:Ny
+    jacobian_exact[i, j] = ∂f_x(i, j, 1, grid, xᶠ, yᶜ) * ∂g_y(i, j, 1, grid, xᶜ, yᶠ) 
+                         - ∂f_y(i, j, 1, grid, xᶜ, yᶠ) * ∂g_x(i, j, 1, grid, xᶠ, yᶜ)
 end
 
-
-error = abs.(jacobian .- jacobian_approx)
-
+error = abs.(jacobian_exact .- jacobian_approx)
 
 plt_jacobian = plot_surface(xᶜ, yᶜ, jacobian)
 plt_jacobian_approx = plot_surface(xᶜ, yᶜ, jacobian_approx)
