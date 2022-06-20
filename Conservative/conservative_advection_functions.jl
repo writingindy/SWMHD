20729a336c37f9ac7414082e2ed9c8a193c26951
Indy Ng <writingindy@gmail.com>
2022-06-20 15:34:51 -0400

    Add initial conservative formulation SWMHD

---
+++ Conservative/conservative_advection_functions.jl
@@ -0,0 +1,38 @@
+function B_x(i, j, k, grid, A, h)
+    return -ℑxyᶠᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, A) / ℑxᶠᵃᵃ(i, j, k, grid, h)
+end
+
+function B_y(i, j, k, grid, A, h)
+    return ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A) / ℑyᵃᶠᵃ(i, j, k, grid, h)
+end
+
+function hB_x(i, j, k, grid, A)
+    return -ℑxyᶠᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, A)
+end
+
+function hB_y(i, j, k, grid, A)
+    return ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A)
+end
+
+@inline momentum_flux_hbx_bx(i, j, k, grid, advection, fields) = 
+    @inbounds _advective_momentum_flux_Uu(i, j, k, grid, advection, hB_x, B_x, fields.A, fields.h)
+
+@inline momentum_flux_hby_bx(i, j, k, grid, advection, fields) =
+    @inbounds _advective_momentum_flux_Vu(i, j, k, grid, advection, hB_y, B_x, fields.A, fields.h)
+
+@inline momentum_flux_hbx_by(i, j, k, grid, advection, fields) =
+    @inbounds _advective_momentum_flux_Uv(i, j, k, grid, advection, hB_x, B_y, fields.A, fields.h)
+
+@inline momentum_flux_hby_by(i, j, k, grid, advection, fields) =
+    @inbounds _advective_momentum_flux_Vv(i, j, k, grid, advection, hB_y, B_y, fields.A, fields.h)
+
+
+
+function div_lorentz_x(i, j, k, grid, advection, fields)
+    return 1 / Vᶠᶜᶜ(i, j, k, grid) * (δxᶠᵃᵃ(i, j, k, grid, momentum_flux_hbx_bx, advection, fields) + δyᵃᶜᵃ(i, j, k, grid, momentum_flux_hby_bx, advection, fields))
+end
+
+function div_lorentz_y(i, j, k, grid, advection, fields)
+    return 1 / Vᶜᶠᶜ(i, j, k, grid) * (δxᶜᵃᵃ(i, j, k, grid, momentum_flux_hbx_by, advection, fields) + δyᵃᶠᵃ(i, j, k, grid, momentum_flux_hby_by, advection, fields))
+end
+
---
+++ Conservative/conservative_sw_mhd.jl
@@ -0,0 +1,79 @@
+using Oceananigans
+using Oceananigans.Models.ShallowWaterModels: ConservativeFormulation
+using Oceananigans.Advection: VelocityStencil, VorticityStencil
+using Oceananigans.Operators: ℑxᶜᵃᵃ, ∂xᶠᶜᶜ, ℑyᵃᶜᵃ, ∂yᶜᶠᶜ, ℑxyᶠᶜᵃ, ℑxyᶜᶠᵃ, ℑxᶠᵃᵃ, ℑyᵃᶠᵃ, δxᶜᵃᵃ, δyᵃᶜᵃ, Vᶜᶜᶜ
+using CairoMakie, Statistics, JLD2, Printf
+
+
+using Oceananigans.Advection: 
+    _advective_momentum_flux_Uu,
+    _advective_momentum_flux_Uv,
+    _advective_momentum_flux_Vu,
+    _advective_momentum_flux_Vv
+
+using Oceananigans.Grids: AbstractGrid
+using Oceananigans.Operators: Ax_qᶠᶜᶜ, Ay_qᶜᶠᶜ
+
+include("new_functions.jl")
+
+Lx, Ly = 10, 10
+
+grid = RectilinearGrid(size = (64, 64), 
+                          x = (-Lx/2, Lx/2), y = (-Ly/2, Ly/2), 
+                   topology = (Periodic, Periodic, Flat))
+
+model = ShallowWaterModel(grid = grid,
+                          timestepper = :RungeKutta3,
+                          momentum_advection = WENO5(),
+                          mass_advection = WENO5(),
+                          tracer_advection = WENO5(),
+                          gravitational_acceleration = 9.81,
+                          coriolis = FPlane(f=1),
+                          tracers = (:A),
+                          forcing = (u = Forcing(div_lorentz_x, discrete_form = true), 
+                                     v = Forcing(div_lorentz_y, discrete_form = true)),
+                          formulation = ConservativeFormulation()
+                          )
+
+Aᵢ(x, y, z) = 0.1exp(-((x - 0.5)^2 + y^2)) - 0.5exp(-((x + 0.5)^2 + y^2))
+
+#uᵢ(x, y, z) = 5y*exp(-(x^2 + y^2))
+#vᵢ(x, y, z) = -5x*exp(-(x^2 + y^2))
+set!(model#=, u = uᵢ, v = vᵢ=#, h = 1, A = Aᵢ)
+simulation = Simulation(model, Δt = 0.01, stop_time = 35.0)
+
+start_time = [time_ns()]
+
+function progress(sim)
+    wall_time = (time_ns() - start_time[1]) * 1e-9
+
+    u = sim.model.solution.u
+    h = sim.model.solution.h
+    A = sim.model.tracers.A 
+
+    @info @sprintf("Time: % 12s, iteration: %d, max(|u|): %.2e ms⁻¹, max(A): %.2e ms⁻¹, min(h): %.2e ms⁻¹, wall time: %s",
+                    prettytime(sim.model.clock.time),
+                    sim.model.clock.iteration, 
+                    maximum(abs, u), maximum(abs, A), minimum(h),
+                    prettytime(wall_time))
+
+    start_time[1] = time_ns()
+
+    return nothing
+end
+
+simulation.callbacks[:progress] = Callback(progress, IterationInterval(1))
+
+u, v, h = model.solution
+s = sqrt(u^2 + v^2)
+compute!(s)
+
+filename = "SW_MHD_adjustment"
+
+simulation.output_writers[:fields] = JLD2OutputWriter(model, (; u, v, A = model.tracers.A, s),
+                                                      schedule = TimeInterval(0.1),
+                                                      filename = filename * ".jld2",
+                                                      overwrite_existing = true)
+
+@info "Running with Δt = $(prettytime(simulation.Δt))"
+run!(simulation)
