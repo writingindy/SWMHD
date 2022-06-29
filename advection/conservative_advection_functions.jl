0bc1fc5f1f032b47a532d8d0164b69e60d4889a8
Indy Ng <writingindy@gmail.com>
2022-06-29 15:48:57 -0400

    Fixed advective and jacobian formulations

--- advection/conservative_advection_functions.jl
+++ advection/conservative_advection_functions.jl
@@ -5,10 +5,8 @@
 @inline symmetric_interpolate_xᶠᵃᵃ(i, j, k, grid, c, args...) = ℑxᶠᵃᵃ(i, j, k, grid, c, args...)
 @inline symmetric_interpolate_yᵃᶠᵃ(i, j, k, grid, c, args...) = ℑyᵃᶠᵃ(i, j, k, grid, c, args...)
 
-
 @inline symmetric_interpolate_xᶜᵃᵃ(i, j, k, grid, u, args...) = ℑxᶜᵃᵃ(i, j, k, grid, u, args...)
 @inline symmetric_interpolate_yᵃᶜᵃ(i, j, k, grid, v, args...) = ℑyᵃᶜᵃ(i, j, k, grid, v, args...)
-
 
 @inline left_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, c, args...) = @inbounds (2 * c(i, j, k, grid, args...) + 5 * c(i-1, j, k, grid, args...) - c(i-2, j, k, grid, args...)) / 6
 @inline left_biased_interpolate_yᵃᶠᵃ(i, j, k, grid, c, args...) = @inbounds (2 * c(i, j, k, grid, args...) + 5 * c(i, j-1, k, grid, args...) - c(i, j-2, k, grid, args...)) / 6
@@ -22,7 +20,7 @@
 @inline right_biased_interpolate_xᶜᵃᵃ(i, j, k, grid, u, args...) = right_biased_interpolate_xᶠᵃᵃ(i+1, j, k, grid, u, args...)
 @inline right_biased_interpolate_yᵃᶜᵃ(i, j, k, grid, v, args...) = right_biased_interpolate_yᵃᶠᵃ(i, j+1, k, grid, v, args...)
 
-@inline function advective_momentum_flux_Uu(i, j, k, grid, U, u, args...)
+@inline function advective_lorentz_flux_hBx_bx(i, j, k, grid, U, u, args...)
 
     ũ  =    symmetric_interpolate_xᶜᵃᵃ(i, j, k, grid, U, args...)
     uᴸ =  left_biased_interpolate_xᶜᵃᵃ(i, j, k, grid, u, args...)
@@ -31,7 +29,7 @@
     return Axᶜᶜᶜ(i, j, k, grid) * upwind_biased_product(ũ, uᴸ, uᴿ)
 end
 
-@inline function advective_momentum_flux_Vu(i, j, k, grid, V, u, args...)
+@inline function advective_lorentz_flux_hBy_bx(i, j, k, grid, V, u, args...)
 
     ṽ  =    symmetric_interpolate_xᶠᵃᵃ(i, j, k, grid, V, args...)
     uᴸ =  left_biased_interpolate_yᵃᶠᵃ(i, j, k, grid, u, args...)
@@ -40,7 +38,7 @@ end
     return Ayᶠᶠᶜ(i, j, k, grid) * upwind_biased_product(ṽ, uᴸ, uᴿ)
 end
 
-@inline function advective_momentum_flux_Uv(i, j, k, grid, U, v, args...)
+@inline function advective_lorentz_flux_hBx_by(i, j, k, grid, U, v, args...)
 
     ũ  =    symmetric_interpolate_yᵃᶠᵃ(i, j, k, grid, U, args...)
     vᴸ =  left_biased_interpolate_xᶠᵃᵃ(i, j, k, grid, v, args...)
@@ -49,7 +47,7 @@ end
     return Axᶠᶠᶜ(i, j, k, grid) * upwind_biased_product(ũ, vᴸ, vᴿ)
 end
 
-@inline function advective_momentum_flux_Vv(i, j, k, grid, V, v, args...)
+@inline function advective_lorentz_flux_hBy_by(i, j, k, grid, V, v, args...)
 
     ṽ  =    symmetric_interpolate_yᵃᶜᵃ(i, j, k, grid, V, args...)
     vᴸ =  left_biased_interpolate_yᵃᶜᵃ(i, j, k, grid, v, args...)
@@ -58,40 +56,42 @@ end
     return Ayᶜᶜᶜ(i, j, k, grid) * upwind_biased_product(ṽ, vᴸ, vᴿ)
 end
 
-function B_x(i, j, k, grid, A, h)
+function Bx(i, j, k, grid, A, h)
     return -ℑxyᶠᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, A) / ℑxᶠᵃᵃ(i, j, k, grid, h)
 end
 
-function B_y(i, j, k, grid, A, h)
+function By(i, j, k, grid, A, h)
     return ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A) / ℑyᵃᶠᵃ(i, j, k, grid, h)
 end
 
-function hB_x(i, j, k, grid, A, h)
+function hBx(i, j, k, grid, A, h)
     return -ℑxyᶠᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, A)
 end
 
-function hB_y(i, j, k, grid, A, h)
+function hBy(i, j, k, grid, A, h)
     return ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A)
 end
 
-@inline momentum_flux_hbx_bx(i, j, k, grid, fields) = 
-    @inbounds advective_momentum_flux_Uu(i, j, k, grid, hB_x, B_x, fields.A, fields.h) / fields.h[i, j, k]
+@inline lorentz_flux_hBx_bx(i, j, k, grid, fields) = 
+    @inbounds advective_lorentz_flux_hBx_bx(i, j, k, grid, hBx, Bx, fields.A, fields.h)
 
-@inline momentum_flux_hby_bx(i, j, k, grid, fields) =
-    @inbounds advective_momentum_flux_Vu(i, j, k, grid, hB_y, B_x, fields.A, fields.h) / ℑxyᶠᶠᵃ(i, j, k, grid, fields.h)
+@inline lorentz_flux_hBy_bx(i, j, k, grid, fields) =
+    @inbounds advective_lorentz_flux_hBy_bx(i, j, k, grid, hBy, Bx, fields.A, fields.h)
 
-@inline momentum_flux_hbx_by(i, j, k, grid, fields) =
-    @inbounds advective_momentum_flux_Uv(i, j, k, grid, hB_x, B_y, fields.A, fields.h) / ℑxyᶠᶠᵃ(i, j, k, grid, fields.h)
+@inline lorentz_flux_hBx_by(i, j, k, grid, fields) =
+    @inbounds advective_lorentz_flux_hBx_by(i, j, k, grid, hBx, By, fields.A, fields.h)
 
-@inline momentum_flux_hby_by(i, j, k, grid, fields) =
-    @inbounds advective_momentum_flux_Vv(i, j, k, grid, hB_y, B_y, fields.A, fields.h) / fields.h[i, j, k]
+@inline lorentz_flux_hBy_by(i, j, k, grid, fields) =
+    @inbounds advective_lorentz_flux_hBy_by(i, j, k, grid, hBy, By, fields.A, fields.h)
 
 function div_lorentz_x(i, j, k, grid, clock, fields)
-    return (1/Azᶠᶜᶜ(i, j, k, grid)) * (δxᶠᵃᵃ(i, j, k, grid, momentum_flux_hbx_bx, fields) + δyᵃᶜᵃ(i, j, k, grid, momentum_flux_hby_bx, fields))
+    return ((1/Azᶠᶜᶜ(i, j, k, grid)) * (δxᶠᵃᵃ(i, j, k, grid, lorentz_flux_hBx_bx, fields) 
+                                     + δyᵃᶜᵃ(i, j, k, grid, lorentz_flux_hBy_bx, fields)))
 end
 
 function div_lorentz_y(i, j, k, grid, clock, fields)
-    return (1/Azᶜᶠᶜ(i, j, k, grid)) * (δxᶜᵃᵃ(i, j, k, grid, momentum_flux_hbx_by, fields) + δyᵃᶠᵃ(i, j, k, grid, momentum_flux_hby_by, fields))
+    return ((1/Azᶜᶠᶜ(i, j, k, grid)) * (δxᶜᵃᵃ(i, j, k, grid, lorentz_flux_hBx_by, fields) 
+                                     + δyᵃᶠᵃ(i, j, k, grid, lorentz_flux_hBy_by, fields)))
 end
 
 
--- jacobian/sw_mhd_jacobian_functions.jl
+++ jacobian/sw_mhd_jacobian_functions.jl
@@ -15,23 +15,23 @@ function ∂y∂yA_over_h(i, j, k, grid, A, h)
 end
 
 function jacobian_x(i, j, k, grid, fields)
-    return (∂xᶠᶜᶜ(i, j, k, grid, fields.A)       
+    return ((∂xᶠᶜᶜ(i, j, k, grid, fields.A)       
          * ℑxyᶠᶜᵃ(i, j, k, grid, ∂y∂yA_over_h, fields.A, fields.h)) 
-        - (ℑxyᶠᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, fields.A) 
+        - (ℑxyᶠᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, fields.A) )
           * ∂xᶠᶜᶜ(i, j, k, grid, ∂yA_over_h, fields.A, fields.h))
 end
 
 function jacobian_y(i, j, k, grid, fields)
-    return (ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, fields.A) 
+    return ((ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, fields.A) 
            * ∂yᶜᶠᶜ(i, j, k, grid, ∂xA_over_h, fields.A, fields.h)) 
           - (∂yᶜᶠᶜ(i, j, k, grid, fields.A)        
-          * ℑxyᶜᶠᵃ(i, j, k, grid, ∂x∂xA_over_h, fields.A, fields.h))
+          * ℑxyᶜᶠᵃ(i, j, k, grid, ∂x∂xA_over_h, fields.A, fields.h)))
 end
 
 function lorentz_force_func_x(i, j, k, grid, clock, fields)
-    return (-1/ℑxᶠᵃᵃ(i, j, k, grid, fields.h))*(jacobian_x(i, j, k, grid, fields))
+    return (1/ℑxᶠᵃᵃ(i, j, k, grid, fields.h))*(jacobian_x(i, j, k, grid, fields))
 end
 
 function lorentz_force_func_y(i, j, k, grid, clock, fields)
-    return (1/ℑyᵃᶠᵃ(i, j, k, grid, fields.h))*(jacobian_y(i, j, k, grid, fields))
+    return (-1/ℑyᵃᶠᵃ(i, j, k, grid, fields.h))*(jacobian_y(i, j, k, grid, fields))
 end
