30268441970b775274ee9b1c40f30804b1d2eb17
Indy Ng <writingindy@gmail.com>
2022-06-30 17:09:57 -0400

    Add simplified jacobian formulation functions

--- jacobian/sw_mhd_jacobian_functions.jl
+++ jacobian/sw_mhd_jacobian_functions.jl
@@ -1,37 +1,26 @@
-function ∂xA_over_h(i, j, k, grid, A, h)
-    return ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A)/h[i, j, k]
+function By(i, j, k, grid, A, h)
+    return ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A, args...)/h[i, j, k]
 end
 
-function ∂yA_over_h(i, j, k, grid, A, h)
-    return ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, A)/h[i, j, k]
+function Bx(i, j, k, grid, A, h)
+    return -ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, A, args...)/h[i, j, k]
 end
 
-function ∂x∂xA_over_h(i, j, k, grid, A, h)
-    return ∂xᶠᶜᶜ(i, j, k, grid, ∂xA_over_h, A, h)
+
+function jacobian_x(i, j, k, grid, A, h)
+    return ((∂xᶠᶜᶜ(i, j, 1, grid, A, h)*ℑxyᶠᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, Bx, A, h)) 
+            - (ℑxyᶠᶜᵃ(i, j, 1, grid, ∂yᶜᶠᶜ, A, h)*∂xᶠᶜᶜ(i, j, k, grid, Bx, A, h)))
 end
 
-function ∂y∂yA_over_h(i, j, k, grid, A, h)
-    return ∂yᶜᶠᶜ(i, j, k, grid, ∂yA_over_h, A, h)
-end
-
-function jacobian_x(i, j, k, grid, fields)
-    return ((∂xᶠᶜᶜ(i, j, k, grid, fields.A)       
-         * ℑxyᶠᶜᵃ(i, j, k, grid, ∂y∂yA_over_h, fields.A, fields.h)) 
-        - (ℑxyᶠᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, fields.A) )
-          * ∂xᶠᶜᶜ(i, j, k, grid, ∂yA_over_h, fields.A, fields.h))
-end
-
-function jacobian_y(i, j, k, grid, fields)
-    return ((ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, fields.A) 
-           * ∂yᶜᶠᶜ(i, j, k, grid, ∂xA_over_h, fields.A, fields.h)) 
-          - (∂yᶜᶠᶜ(i, j, k, grid, fields.A)        
-          * ℑxyᶜᶠᵃ(i, j, k, grid, ∂x∂xA_over_h, fields.A, fields.h)))
+function jacobian_y(i, j, k, grid, A, h)
+    return ((ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A, h)*∂yᶜᶠᶜ(i, j, k, grid, By, A, h))
+            - (∂yᶜᶠᶜ(i, j, k, grid, A, h)*ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, By, A, h)))
 end
 
 function lorentz_force_func_x(i, j, k, grid, clock, fields)
-    return (1/ℑxᶠᵃᵃ(i, j, k, grid, fields.h))*(jacobian_x(i, j, k, grid, fields))
+    return (1/ℑxᶠᵃᵃ(i, j, k, grid, fields.h))*(jacobian_x(i, j, k, grid, fields.A, fields.h))
 end
 
 function lorentz_force_func_y(i, j, k, grid, clock, fields)
-    return (-1/ℑyᵃᶠᵃ(i, j, k, grid, fields.h))*(jacobian_y(i, j, k, grid, fields))
+    return (1/ℑyᵃᶠᵃ(i, j, k, grid, fields.h))*(jacobian_y(i, j, k, grid, fields.A, fields.h))
 end
