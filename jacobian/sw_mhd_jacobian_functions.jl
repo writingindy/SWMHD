eccf0351c2cfb18ce508e03b6271ab4c2ce99a45
Indy Ng <writingindy@gmail.com>
2022-07-03 13:27:16 -0400

    Minor fix to jacobian formulation

--- jacobian/sw_mhd_jacobian_functions.jl
+++ jacobian/sw_mhd_jacobian_functions.jl
@@ -1,20 +1,20 @@
 function By(i, j, k, grid, A, h)
-    return ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A, args...)/h[i, j, k]
+    return ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A)/h[i, j, k]
 end
 
 function Bx(i, j, k, grid, A, h)
-    return -ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, A, args...)/h[i, j, k]
+    return -ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, A)/h[i, j, k]
 end
 
 
 function jacobian_x(i, j, k, grid, A, h)
-    return ((∂xᶠᶜᶜ(i, j, 1, grid, A, h)*ℑxyᶠᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, Bx, A, h)) 
-            - (ℑxyᶠᶜᵃ(i, j, 1, grid, ∂yᶜᶠᶜ, A, h)*∂xᶠᶜᶜ(i, j, k, grid, Bx, A, h)))
+    return ((∂xᶠᶜᶜ(i, j, 1, grid, A)*ℑxyᶠᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, Bx, A, h)) 
+            - (ℑxyᶠᶜᵃ(i, j, 1, grid, ∂yᶜᶠᶜ, A)*∂xᶠᶜᶜ(i, j, k, grid, Bx, A, h)))
 end
 
 function jacobian_y(i, j, k, grid, A, h)
-    return ((ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A, h)*∂yᶜᶠᶜ(i, j, k, grid, By, A, h))
-            - (∂yᶜᶠᶜ(i, j, k, grid, A, h)*ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, By, A, h)))
+    return ((ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, A)*∂yᶜᶠᶜ(i, j, k, grid, By, A, h))
+            - (∂yᶜᶠᶜ(i, j, k, grid, A)*ℑxyᶜᶠᵃ(i, j, k, grid, ∂xᶠᶜᶜ, By, A, h)))
 end
 
 function lorentz_force_func_x(i, j, k, grid, clock, fields)
